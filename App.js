5927e9e7e4d216ffd1b4c4656f7790b148a56359
olasubomi-o <67253470+olasubomi-o@users.noreply.github.com>
2022-10-08 12:30:51 -0500

    screens and notif

--- App.js
+++ App.js
@@ -58,13 +58,14 @@ export default function App() {
 }
 
 async function schedulePushNotification() {
+  
   await Notifications.scheduleNotificationAsync({
     content: {
       title: "You've got mail! 📬",
       body: 'Here is the notification body',
       data: { data: 'goes here' },
     },
-    trigger: { seconds: 2 },
+    trigger: { seconds: 30 },
   });
 }
 
