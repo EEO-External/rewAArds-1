379efbce80f727f7326bac3256346b29b6b3bd32
olasubomi-o <67253470+olasubomi-o@users.noreply.github.com>
2022-10-08 19:19:16 -0500

    Screens

--- Screens/styles.js
+++ Screens/styles.js
@@ -5,8 +5,39 @@ export default StyleSheet.create({
 container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+    paddingTop: 100
 
-}
+},
+input: {
+    height: 48,
+    borderRadius: 15,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 19,
+    borderColor: 'black',
+    borderWidth: 0.7,
+    textAlign: 'center'
+},
+button: {
+backgroundColor: 'blue',
+marginLeft: 30,
+marginRight: 30,
+marginTop: 20,
+height: 48,
+borderRadius: 15,
+alignItems: "center",
+justifyContent: 'center',
+},
+
+buttonTitle: {
+color: 'white',
+fontSize: 16,
+fontWeight: "bold"
+},
 
 })
